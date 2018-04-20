d40aa80f219fef896b42de2380fad75acbe79a6f
uriel270195 <uriel_270195@hotmail.com>
2018-04-20 14:58:59 -0500

    angular material

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -2,12 +2,19 @@ import { Component, OnInit } from '@angular/core';
 import { GitHubService } from '../services/git-hub.service';
 import { GitHubModel } from '../model/git-hub-model';
 
+
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.css']
 })
 export class MainComponent implements OnInit {
+
+  foods = [
+    {value: 'steak-0', viewValue: 'Steak'},
+    {value: 'pizza-1', viewValue: 'Pizza'},
+    {value: 'tacos-2', viewValue: 'Tacos'}
+  ];
   //gitHubData: GitHubModel;
   gitHubDataList: Array<GitHubModel>;
   //el private _gt: GitHubService evita ser instanciado  con el new y se invoca el _gt.prueba();
