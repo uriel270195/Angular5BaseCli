2ef7be41b59ad44e2ceff00ecc52559209434c9c
uriel270195 <uriel_270195@hotmail.com>
2018-04-30 02:05:26 -0500

    pruebas unitarias

--- src/app/registro-box/registro-box.component.ts
+++ src/app/registro-box/registro-box.component.ts
@@ -23,22 +23,92 @@ export class registroBoxComponent implements OnInit, OnDestroy {
     ngOnInit(){
     }
     acceder(){
-        this.validarNombre(this.nombre) ? alert("Ok") : alert("Error");
+        this.validarNombre(this.nombre) ? 
+            ( this.validarUser(this.user) ? 
+                ( this.validarPassword(this.pass) ? 
+                    ( this.validarConfirmacionPassword(this.pass , this.confpass) ?
+                        ( this.validarCorreo(this.correo) ? 
+                            ( this.validarTerminos(this.terminos_codiciones) ? alert("Ok") : alert("Error Yerminos y Condiciones"))   
+                        : alert("Error correo"))  
+                     : alert("Error Confirmacion password")) 
+                : alert("Error Password")) 
+            : alert("Error Usuario"))
+        : alert("Error Nombre");
             //this._log.login(this.user,this.pass) ? alert("Ok") : alert('Error');
     }
     validarNombre(nom: string){
         let pos=1;
         for (let i = 0; i < nom.length; i++) {
             let valor=nom.substring(i,pos).charCodeAt(0);
-            if(valor<=65 && valor>=90
-            || valor<=97 && valor>=122
-            || valor!=165){
-                return false;
+            if(valor<65 || valor>90){
+                if(valor<97 || valor>122){
+                    if(valor!=32){
+                        return false;
+                    }
+                }
             }
-            //65-90..........97-122
             pos++;
         }
         return true
+    }
+    validarUser(us: string){
+        if(us.length <5 || us.length >15){
+            return false
+        }
+        let pos=1;
+        for (let i = 0; i < us.length; i++) {
+            let valor=us.substring(i,pos).charCodeAt(0);
+            if(valor<65 || valor>90){
+                if(valor<97 || valor>122){
+                    if(valor<48 || valor>57){
+                        return false;
+                    }
+                }
+            }
+            pos++;
+        }
+        return true
+    }
+    validarPassword(pas: string){
+        if(pas.length <2 ){
+            return false
+        }
+        let pos=1,max=0,min=0,num=0;
+        for (let i = 0; i < pas.length; i++) {
+            let valor=pas.substring(i,pos).charCodeAt(0);
+            if((valor>65 || valor<90) && max==0){
+                max++;
+            } else if((valor>97 || valor<122) && min==0){
+                min++;
+            }else if((valor>48 || valor<57) && num==0){
+                num++
+            }
+            //console.log(`valor: ${valor} max: ${max} min: ${min} num: ${num}`);
+            pos++;
+        }
+        if(max==1 && min==1 && num==1){
+            return true
+        }
+        return false
+    }
+    validarConfirmacionPassword(pas: string, confpas: string){
+        if(pas===confpas){
+            return true;
+        } 
+        return false;
+    }
+    validarCorreo(mail: string){
+        if(mail.indexOf(' ')!=-1){
+            return false;
+        }else if(mail.indexOf('@')>=1 && mail.indexOf('.')>mail.indexOf('@')+1){
+            if(mail.length>mail.indexOf('.')+1){
+                return true;
+            }
+            return false;
+        }
+    }
+    validarTerminos(term: Boolean){
+        console.log(term);
     }
 }
 //guard elemento
